3f5f68f492574043293400809b9e2284680d8bfa
Esmail Shabayek <esmail.shabayek@gmail.com>
2019-11-14 22:48:13 +0200

    Add attribute table seeder

--- packages/Attribute/src/Database/Seeders/AttributeTableSeeder.php
+++ packages/Attribute/src/Database/Seeders/AttributeTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Attribute\Database\Seeders;
 
+use Attribute\Models\Attribute;
 use Illuminate\Database\Seeder;
 
 class AttributeTableSeeder extends Seeder
@@ -13,5 +14,22 @@ class AttributeTableSeeder extends Seeder
      */
     public function run()
     {
+        $attribute = Attribute::create([
+            'name' => 'Color'
+        ]);
+        $attribute->options()->createMany([
+            ['label' => 'Red'],
+            ['label' => 'Black'],
+            ['label' => 'Blue'],
+        ]);
+
+        $attribute = Attribute::create([
+            'name' => 'Size'
+        ]);
+        $attribute->options()->createMany([
+            ['label' => 'medium'],
+            ['label' => 'large'],
+            ['label' => 'xlarge'],
+        ]);
     }
 }
