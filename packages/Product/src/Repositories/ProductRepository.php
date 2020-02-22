6875c17435a8e28c40bc90410d0a2db531de1427
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-02-22 13:03:25 +0200

    Hotfix on attribute with create product

--- packages/Product/src/Repositories/ProductRepository.php
+++ packages/Product/src/Repositories/ProductRepository.php
@@ -41,8 +41,6 @@ class ProductRepository extends BaseRepository
         $product = $this->model->create($data);
         $variations = json_decode($data['variations'], true);
 
-        $this->productFlat->createProductFlat($data, $product);
-
         if($data['type'] == 'configurable'){
             $product->attributes()->attach($data['attributes']);
             foreach ($variations as $variant) {
@@ -82,5 +80,43 @@ class ProductRepository extends BaseRepository
         $attributes_ids = Arr::pluck($variant['attributes'], 'id');
         $productVariant->options()->attach($attributes_ids);
         $this->productFlat->createProductFlat($data, $productVariant);
+    }
+    private function uploadImages($productFlat, $product)
+    {
+        if (request()->has('images')) {
+            foreach (request('images') as $key => $image) {
+                // Get filename with the extension
+                $filenameWithExt = $image->getClientOriginalName();
+                // Get just filename
+                $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+                // Get just ext
+                $extension = $image->getClientOriginalExtension();
+                if($key == 'thumb'){
+
+                    $fileNameToStore= 'product-' . $product->id .'.'.$extension;
+                    $path = $image->storeAs('public/products/' . $product->id,  $fileNameToStore);
+                    $fileNameToStore = Str::replaceFirst('public/', '', $path);
+                    $productImage = $productFlat->update([
+                        'thumbnail' => $fileNameToStore
+                    ]);
+
+                }else{
+                    // Filename to store
+                    $fileNameToStore= $filename.'.'.$extension;
+                    // Upload Image
+                    $path = $image->store('public/products/' . $product->id);
+
+                    $fileNameToStore = Str::replaceFirst('public/', '', $path);
+
+                    $productImage = $product->images()->create([
+                        'path' => $fileNameToStore
+                    ]);
+                    $img = Image::make(public_path('storage/'.$productImage->path))->fit(130, 150, null, 'center');
+                    $img->save();
+                }
+
+            }
+        }
+        return $productFlat;
     }
 }
