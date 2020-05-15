a751424432242d6707ab5a9ec701bdd2b0cbb4e8
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-05-15 03:26:24 +0200

    Add payment module structure

---
+++ packages/Payment/Config/Payment.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Driver
+    |--------------------------------------------------------------------------
+    |
+    | This value determines which of the following gateway to use.
+    | You can switch to a different driver at runtime.
+    |
+    */
+    'default' => 'COD'
+
+    ];
---
+++ packages/Payment/Providers/PaymentServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Payment\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaymentServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
