74fcdf9b6ba134a916fd49737ee1118a07048c09
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-05-30 16:20:31 +0200

    Add customer address migration

---
+++ modules/Customer/Database/Migrations/2020_05_30_132914_create_customer_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('address');
+            $table->string('state');
+            $table->string('city');
+            $table->decimal('address_lat');
+            $table->decimal('address_lng');
+            $table->string('postcode');
+            $table->foreignId('customer_id')->references('id')->on('customers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_addresses');
+    }
+}
