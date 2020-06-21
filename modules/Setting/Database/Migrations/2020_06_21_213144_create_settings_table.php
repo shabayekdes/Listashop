8f8b42b8dadd1345f1879224db38a5786f78eac3
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-06-22 00:56:20 +0200

    Add settings table and model

---
+++ modules/Setting/Database/Migrations/2020_06_21_213144_create_settings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('tab_key');
+            $table->string('setting_key')->unique();
+            $table->text('plain_value')->nullable();
+            $table->string('type_field')->default('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
