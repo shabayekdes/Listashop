61477267fcc3d66f9978753bdcabea1c8587e2e0
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-02-06 23:26:50 +0200

    Disable payment config

--- modules/Payment/Providers/PaymentServiceProvider.php
+++ modules/Payment/Providers/PaymentServiceProvider.php
@@ -24,7 +24,7 @@ class PaymentServiceProvider extends ServiceProvider
     public function register()
     {
         $this->registerFacades();
-        $this->registerConfig();
+        // $this->registerConfig();
     }
 
     /**
